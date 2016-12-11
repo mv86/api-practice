ffeaa84774c8a1ed021be5d28884588131941b6e
Max <mveasey87@gmail.com>
2016-12-11 21:35:21 +0000

    added link to film soundtrack on spotify using spotify api

--- public/app.js
+++ public/app.js
@@ -5,9 +5,6 @@ var app = function() {
       var film = this.value;
       makeRequest(film, requestCompleteFilm);
     };
-  // var soundtrackUrl = "https://api.spotify.com/v1/search?q=The+Shawshank+Redemption&type=album&limit=1";
-  // makeRequest(filmUrl, requestCompleteFilm);
-  // makeRequest(soundtrackUrl, requestCompleteSoundtrack)
 };
 
 var makeRequest = function(url, callback) {
@@ -21,30 +18,17 @@ var requestCompleteFilm = function() {
   if(this.status !== 200) return;
   var jsonString = this.responseText;
   var filmJson = JSON.parse(jsonString);
+  soundtrackRequest = "https://api.spotify.com/v1/search?q=" + filmJson.Title + "&type=album&limit=1";
+  makeRequest(soundtrackRequest, requestCompleteSoundtrack);
   populateFilmInfo(filmJson);
 }
 
-// var requestCompleteSoundtrack = function() {
-//   if(this.status !== 200) return;
-//   var jsonString = this.responseText;
-//   var soundtrackJson = JSON.parse(jsonString);
-//   populateSoundtrackInfo(soundtrackJson);
-// }
-
-// var populateSelect = function(films) {
-//   var select = document.getElementById('film-select');
-
-//   select.onchange = function() {
-//     var film = films[this.value];
-//     populateFilmInfo(film);
-//   };
-
-  // for (film of films) {
-  //   var option = document.createElement('option');
-  //   option.innerText = film.Title;
-  //   select.appendChild(option);
-  // }
-// };
+var requestCompleteSoundtrack = function() {
+  if(this.status !== 200) return;
+  var jsonString = this.responseText;
+  var soundtrackJson = JSON.parse(jsonString);
+  populateSoundtrackInfo(soundtrackJson);
+}
 
 var populateFilmInfo = function(film) {
   var filmInfoCell = document.getElementById('film-info-cell');
@@ -57,29 +41,37 @@ var populateFilmInfo = function(film) {
   var liDirector = document.createElement('li');
   var liAwards = document.createElement('li');
   var liSynopsis = document.createElement('li');
-  var button = document.createElement('button');
+  var spotifyLink = document.createElement('li');
+  var a = document.createElement('a');
+  a.id = "spotify-link";
+  spotifyLink.appendChild(a);
   liTitle.innerText = "Title: " + film.Title;
   liReleased.innerText = "Release Date: " + film.Released;
   liLanguage.innerText = "Language: " + film.Language;
   liDirector.innerText = "Director: " + film.Director;
   liAwards.innerText = "Awards: " + film.Awards;
   liSynopsis.innerText = "Synopsis: " + film.Plot;
-  button.innerText = "Listen to Soundtrack on Spotify"
-  // button.onclick = handleButtonClick;
+  a.innerText = "Listen to Soundtrack on Spotify"
   ul.appendChild(liTitle);
   ul.appendChild(liReleased);
   ul.appendChild(liLanguage);
   ul.appendChild(liDirector);
   ul.appendChild(liAwards);
   ul.appendChild(liSynopsis);
+  ul.appendChild(spotifyLink);
   var filmImg = document.getElementById('poster');
   filmImg.src = film.Poster;
-  ul.appendChild(button);
   filmInfoCell.appendChild(filmImg);
   filmInfoCell.appendChild(ul);
   // populatePoster(film);
   populateMap(film);
   populateGraph(film);
+};
+
+var populateSoundtrackInfo = function(soundtrack) {
+  a = document.getElementById('spotify-link');
+  a.href = soundtrack.albums.items[0].external_urls.spotify;
+  a.target = "_blank";
 };
 
 // var populatePoster = function(film) {
@@ -104,36 +96,9 @@ var populateMap = function(film) {
 };
 
 var populateGraph = function(film) {
-  console.log(film);
-  console.log(film.imdbRating);
   filmRating = parseFloat(film.imdbRating);
-  var graphCell = document.getElementById('graph-cell');
-  var chart = new ColumnChart('column', 'Film IMDB Rating', filmRating, [{y: filmRating}],
+  new ColumnChart('column', 'Film IMDB Rating', filmRating, [{y: filmRating}],
     [film.Title], film.imdbRating);
-  graphCell.appendChild(chart);
 };
-
-// var getFilmName = function(film) {
-//   for (item of film) {
-//     return item.Title;
-//   };
-// };
-
-// var top10Films = [
-//   "http://www.omdbapi.com/?t=volver&y=&plot=short&r=json", 
-//   "http://www.omdbapi.com/?t=the+shawshank+redemption&y=&plot=full&r=json", 
-//   "http://www.omdbapi.com/?i=tt0120735&plot=short&r=json"]
-
-// var populateSoundtrackInfo = function(soundtrack) {
-//   var div = document.getElementById('soundtrack-info');
-//   var liName = document.createElement('li');
-//   var soundtrackImg = document.createElement('img');
-//   console.log(soundtrack);
-//   liName.innerText = soundtrack.albums.items[0].name;
-//   soundtrackImg.src = soundtrack.albums.items[0].images[1].url;
-//   div.appendChild(soundtrackImg);
-//   div.appendChild(liName);
-//   // div.appendChild(ul);
-// }
 
 window.onload = app;
